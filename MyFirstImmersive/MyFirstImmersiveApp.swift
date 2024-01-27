a54d7713e5e00a3f9f3d32096110f46a1aeca1f7
Larry Gezelius <lgezelius@gmail.com>
2024-01-27 15:13:41 -0800

    Initial Commit

---
+++ MyFirstImmersive/MyFirstImmersiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstImmersiveApp.swift
+//  MyFirstImmersive
+//
+//  Created by Larry Gezelius on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstImmersiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
